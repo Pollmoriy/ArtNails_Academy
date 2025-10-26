0bdebaa22f79edd91bb104513c684086215372d6
polina <6704523472@my.lerna.by>
2025-10-26 20:13:20 +0300

    Сделала страницу Login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,6 +4,12 @@ auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # Тут позже добавим проверку пользователя
+        return redirect(url_for('main.home'))
+
     return render_template('login.html')
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
